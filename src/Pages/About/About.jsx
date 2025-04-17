80b57b9ade40764c84d7ea5c9a37e0c820c1b6a4
Sebastián Damis <sebastian.damis@gmail.com>
2025-04-17 19:57:29 -0300

    6to commit

--- src/Pages/About/About.jsx
+++ src/Pages/About/About.jsx
@@ -1,14 +1,59 @@
 import React from 'react'
 import NavBar from '../../components/NavBar/NavBar'
+import "./About.css"
+// import setTimeout from './about'
+import "./about.js"
 
 function About() {
+    // setTimeout();
     return (
 
         <>
-        
+
             <NavBar/>
 
-            <h2>About</h2>
+            
+            <h1>403</h1>
+            <div>
+                <p>
+                    &gt; <span>ERROR CODE</span>: "<i>HTTP 403 Forbidden</i>"
+                </p>
+                <p>
+                    &gt; <span>ERROR DESCRIPTION</span>: "
+                    <i>
+                        Access Denied. You Do Not Have The Permission To Access This Page On
+                        This Server
+                    </i>
+                    "
+                </p>
+                <p>
+                    &gt; <span>ERROR POSSIBLY CAUSED BY</span>: [
+                    <b>
+                        execute access forbidden, read access forbidden, write access forbidden,
+                        ssl required, ssl 128 required, ip address rejected, client certificate
+                        required, site access denied, too many users, invalid configuration,
+                        password change, mapper denied access, client certificate revoked,
+                        directory listing denied, client access licenses exceeded, client
+                        certificate is untrusted or invalid, client certificate has expired or
+                        is not yet valid, passport logon failed, source access denied, infinite
+                        depth is denied, too many requests from the same client ip
+                    </b>
+                    ...]
+                </p>
+                <p>
+                    &gt;{" "}
+                    <span>
+                        SOME PAGES ON THIS SERVER THAT YOU DO HAVE PERMISSION TO ACCESS
+                    </span>
+                        : [<a href="/">Home Page</a>, <a href="/">About Us</a>,{" "}
+                    <a href="/">Contact Us</a>, <a href="/">Blog</a>...]
+                </p>
+                <p>
+                    &gt; <span>HAVE A NICE DAY SIR AXLEROD :-)</span>
+                </p>
+            </div>
+
+
 
         </>
 
---
+++ src/Pages/About/about.js
@@ -0,0 +1,18 @@
+
+let str = document.getElementsByTagName('div')[0].innerHTML.toString();
+let i = 0;
+document.getElementsByTagName('div')[0].innerHTML = "";
+
+setTimeout(function() {
+    let se = setInterval(function() {
+        i++;
+        document.getElementsByTagName('div')[0].innerHTML = str.slice(0, i) + "|";
+        if (i == str.length) {
+            clearInterval(se);
+            document.getElementsByTagName('div')[0].innerHTML = str;
+        }
+    }, 10);
+},0);
+
+
+export default setTimeout
